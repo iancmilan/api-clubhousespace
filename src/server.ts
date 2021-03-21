68a8e43da547774ec870d40597ff93b1b9f6f360
ian <iancmilan@unochapeco.edu.br>
2021-03-20 23:23:02 -0300

    últimas alterações para o deploy 🔧

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-  // eslint-disable-next-line no-console
-  console.log('🚀 Server starded on port 3333!');
-});
+app.listen(process.env.PORT || 3333);
