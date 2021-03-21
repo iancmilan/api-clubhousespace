d9937cef7c1a90867aaf19de41d91c56c7d9b92e
ian <iancmilan@unochapeco.edu.br>
2021-03-21 00:01:03 -0300

    alteração no cors 🛠

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://web-clubhousespace.vercel.app/',
+    origin: 'https://web-clubhousespace.vercel.app',
   }),
 );
 app.use(express.json());
